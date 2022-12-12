e505fd1da0d9b972f0bbfaef82d56003221711b7
davemers0160 <davemers0160@gmail.com>
2022-12-12 11:44:51 -0500

    adding more indepth example with replay

---
+++ pytorch_mario_rl/main.py
@@ -0,0 +1,95 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+import random, datetime
+from pathlib import Path
+
+import gym
+import gym_super_mario_bros
+from gym.wrappers import FrameStack, GrayScaleObservation, TransformObservation
+from nes_py.wrappers import JoypadSpace
+
+from metrics import MetricLogger
+from agent import Mario
+from wrappers import ResizeObservation, SkipFrame
+
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+
+# Initialize Super Mario environment
+env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
+
+# Limit the action-space to
+#   0. walk right
+#   1. jump right
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+#     env,
+#     [['right'],
+#     ['right', 'A']]
+# )
+
+
+# Apply Wrappers to environment
+env = SkipFrame(env, skip=4)
+env = GrayScaleObservation(env, keep_dim=False)
+env = ResizeObservation(env, shape=84)
+env = TransformObservation(env, f=lambda x: x / 255.)
+env = FrameStack(env, num_stack=4)
+
+env.reset()
+
+save_dir = Path('checkpoints') / datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
+save_dir.mkdir(parents=True)
+
+# use this line if you don't want to load from a checkpoint
+# checkpoint = None
+# use something like this to load previous progress from the supplied checkpoint
+checkpoint = Path('checkpoints/2022-12-11T19-36-49/mario_net_1.chkpt')
+
+mario = Mario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir, checkpoint=checkpoint)
+
+logger = MetricLogger(save_dir)
+
+# number of episodes to train
+episodes = 20000
+
+### for Loop that train the model num_episodes times by playing the game
+for e in range(episodes):
+
+    state = env.reset()
+
+    # Play the game!
+    while True:
+
+        # 3. Show environment (the visual) [WIP] - Comment out to run a little faster
+        env.render()
+
+        # 4. Run agent on the state
+        action = mario.act(state)
+
+        # 5. Agent performs action
+        next_state, reward, done, info = env.step(action)
+
+        # 6. Remember
+        mario.cache(state, next_state, action, reward, done)
+
+        # 7. Learn
+        q, loss = mario.learn()
+
+        # 8. Logging
+        logger.log_step(reward, loss, q)
+
+        # 9. Update state
+        state = next_state
+
+        # 10. Check if end of game
+        if done or info['flag_get']:
+            break
+
+    logger.log_episode()
+
+    if e % 20 == 0:
+        logger.record(
+            episode=e,
+            epsilon=mario.exploration_rate,
+            step=mario.curr_step
+        )
